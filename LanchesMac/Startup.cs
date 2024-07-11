2ad9ddfdb30a713cbe17af5d1da84727a9fe3140
Jocemar Hoppe Pereira <jocemarpere@gmail.com>
2024-07-10 21:55:00 -0300

    Criação do arquivo startup e remoção da view privacy

---
+++ LanchesMac/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
+
+namespace LanchesMac;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+    {
+        if (environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(  
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
+
