b3eb8cd0ddadc404f8624691da047d77605261f1
Jocemar Hoppe Pereira <jocemarpere@gmail.com>
2024-07-31 21:55:56 -0300

    Criação da Interface e da Classe Repository da Categoria

---
+++ LanchesMac/Migrations/20240725202312_PopularLaches.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    public partial class PopularLaches : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId,DescricaoCurta,DescricaoDetalhada,EmEstoque,ImagemThumbnailUrl," +
+                "ImagemUrl,IsLanchePreferido,Nome,Preco) VALUES(1, 'Pão, hamburger, ovo, presunto, queijo e batata palha'," +
+                "'delicioso pão de hambúrger com ovo frito; prejunto e queijo de primeira qualidade acompanhado com batata palha'," +
+                "1,'http://www.macoratti.net/Imagens/lanches/cheeseslada1.jpg','http://www.macoratti.net/Imagens/lanches/cheeseslada1.jpg'," +
+                "0,'Cheese Salada', 12.50)");
+            migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId,DescricaoCurta,DescricaoDetalhada,EmEstoque,ImagemThumbnailUrl," +
+                "ImagemUrl,IsLanchePreferido,Nome,Preco) VALUES(1, 'Pão, hamburger, ovo, presunto, queijo e batata palha'," +
+                "'delicioso pão de hambúrger com ovo frito; prejunto e queijo de primeira qualidade acompanhado com batata palha'," +
+                "1,'http://www.macoratti.net/Imagens/lanches/mistoquente4.jpg','http://www.macoratti.net/Imagens/lanches/mistoquente4.jpg'," +
+                "0,'Cheese Salada', 8.00)");
+            migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId,DescricaoCurta,DescricaoDetalhada,EmEstoque,ImagemThumbnailUrl," +
+                "ImagemUrl,IsLanchePreferido,Nome,Preco) VALUES(1, 'Pão, hamburger, ovo, presunto, queijo e batata palha'," +
+                "'delicioso pão de hambúrger com ovo frito; prejunto e queijo de primeira qualidade acompanhado com batata palha'," +
+                "1,'http://www.macoratti.net/Imagens/lanches/cheeseburguer1.jpg','http://www.macoratti.net/Imagens/lanches/cheeseburguer1.jpg'," +
+                "0,'Cheese Salada', 11.00)");
+            migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId,DescricaoCurta,DescricaoDetalhada,EmEstoque,ImagemThumbnailUrl," +
+                "ImagemUrl,IsLanchePreferido,Nome,Preco) VALUES(2, 'Pão, hamburger, ovo, presunto, queijo e batata palha'," +
+                "'delicioso pão de hambúrger com ovo frito; prejunto e queijo de primeira qualidade acompanhado com batata palha'," +
+                "1,'http://www.macoratti.net/Imagens/lanches/lanchenatural1.jpg','http://www.macoratti.net/Imagens/lanches/lanchenatural1.jpg'," +
+                "1,'Cheese Salada', 15.00)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Lanches");
+        }
+    }
+}
--- LanchesMac/Models/Categoria.cs
+++ LanchesMac/Models/Categoria.cs
@@ -1,11 +1,23 @@
-﻿using System.Globalization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
 
 namespace LanchesMac.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+
+        [StringLength(100, ErrorMessage ="O tamanho maximo é de 100 caracteres.")]
+        [Required(ErrorMessage = "Informe o nome da categoria.")]
+        [Display(Name ="Nome")]
         public string CategoriaNome { get; set; }
+
+        [StringLength(200, ErrorMessage = "O tamanho maximo é de 200 caracteres.")]
+        [Required(ErrorMessage = "Informe o nome da Descrição.")]
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
 
         public List<Lanche> Lanches { get; set; }
